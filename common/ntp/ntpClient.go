1a6dd0344fe9ebcd646419e8dde49c0d33e80157
Timur <timhod06@gmail.com>
2024-09-18 10:51:40 +0300

    Fix ntpClient

--- common/ntp/ntpClient.go
+++ common/ntp/ntpClient.go
@@ -1,74 +1,76 @@
 package ntp
 
 import (
-	"github.com/beevik/ntp"
-	"log"
-	"sync"
-	"sync/atomic"
-	"time"
+    "fmt"
+    "github.com/beevik/ntp"
+    "log"
+    "sync"
+    "sync/atomic"
+    "time"
 )
 
 type NTPClient struct {
-	ntpServers      []string
-	offset          time.Duration
-	mutex           sync.RWMutex
-	lastSynced      time.Time
-	synced          atomic.Bool
-	syncAttemptsDone atomic.Bool
+    ntpServers       []string
+    offset           time.Duration
+    mutex            sync.Mutex
+    lastSynced       time.Time
+    synced           atomic.Bool
+    syncAttemptsDone atomic.Bool
 }
 
 func NewNTPClient(ntpServers ...string) *NTPClient {
-	client := &NTPClient{
-		ntpServers: ntpServers,
-	}
-	go client.UpdateTime()
-	return client
+    client := &NTPClient{
+        ntpServers: ntpServers,
+    }
+    go client.UpdateTime()
+    return client
 }
 
-func (c *NTPClient) UpdateTime() {
-	if c.syncAttemptsDone.Load() {
-		return
-	}
+func (c *NTPClient) UpdateTime() error {
+    c.mutex.Lock()
+    defer c.mutex.Unlock()
 
-	var wg sync.WaitGroup
-	for _, server := range c.ntpServers {
-		wg.Add(1)
-		go func(server string) {
-			defer wg.Done()
-			response, err := ntp.Query(server)
-			if err != nil {
-				log.Printf("Error querying NTP server %s: %v", server, err)
-				return
-			}
-			if err := response.Validate(); err != nil {
-				log.Printf("Validation error from NTP server %s: %v", server, err)
-				return
-			}
+    if c.syncAttemptsDone.Load() {
+        return nil
+    }
 
-			c.mutex.Lock()
-			defer c.mutex.Unlock()
-			c.offset = response.ClockOffset
-			c.lastSynced = time.Now()
-			c.synced.Store(true)
-			c.syncAttemptsDone.Store(true)
-			log.Printf("Time synchronized with NTP server %s, offset: %v", server, c.offset)
-		}(server)
-	}
+    var wg sync.WaitGroup
+    for _, server := range c.ntpServers {
+        wg.Add(1)
+        go func(server string) {
+            defer wg.Done()
+            response, err := ntp.Query(server)
+            if err != nil {
+                log.Printf("Error querying NTP server %s: %v", server, err)
+                return
+            }
+            err = response.Validate()
+            if err != nil {
+                log.Printf("Validation error from NTP server %s: %v", server, err)
+                return
+            }
+            c.offset = response.ClockOffset
+            c.lastSynced = time.Now()
+            c.synced.Store(true)
+            c.syncAttemptsDone.Store(true)
+            log.Printf("Time synchronized with NTP server %s", server)
+        }(server)
+    }
+    wg.Wait()
 
-	wg.Wait()
-
-	if !c.synced.Load() {
-		log.Println("Failed to synchronize time with all NTP servers")
-	}
+    if !c.synced.Load() {
+        return fmt.Errorf("failed to synchronize time with all provided NTP servers")
+    }
+    return nil
 }
 
 func (c *NTPClient) Now() time.Time {
-	if !c.synced.Load() && !c.syncAttemptsDone.Load() {
-		go c.UpdateTime()
-		return time.Now()
-	}
+    if !c.synced.Load() && !c.syncAttemptsDone.Load() {
+        go c.UpdateTime()
+        if !c.synced.Load() {
+            return time.Now()
+        }
+    }
 
-	c.mutex.RLock()
-	defer c.mutex.RUnlock()
-	return time.Now().Add(c.offset)
+    return time.Now().Add(c.offset)
 }
